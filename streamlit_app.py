73661d5c4d51b856696f47fce763c02b3572d6a3
mutiaraanjani-droid <mutiaramuti398@gmail.com>
2026-05-19 11:21:22 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,3 +19,11 @@ a soft return is used for the next line.
 Two (or more) newline characters in a row will result in a hard return.
 '''
 st.markdown(multi)
+
+import streamlit as st
+
+st.latex(r'''
+    a + ar + a r^2 + a r^3 + \cdots + a r^{n-1} =
+    \sum_{k=0}^{n-1} ar^k =
+    a \left(\frac{1-r^{n}}{1-r}\right)
+    ''')
